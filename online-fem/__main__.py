18b31b87d9cd81038e3a0d53ae4fc30742972c85
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-27 23:49:23 +0900

    Switch to hello world

--- online-fem/__main__.py
+++ online-fem/__main__.py
@@ -6,35 +6,44 @@ The purpose of this app is to demonstrate that Panel works with the tools you kn
 import panel as pn
 import pyvista as pv
 
-# from awesome_panel import config
+pv.set_plot_theme("document")
 
-# config.extension("vtk", url="lib_pyvista")
+cyl = pv.Cylinder()
+arrow = pv.Arrow()
+sphere = pv.Sphere()
+plane = pv.Plane()
+line = pv.Line()
+box = pv.Box()
+cone = pv.Cone()
+poly = pv.Polygon()
+disc = pv.Disc()
 
-# THEME = config.get_theme()
-# ACCENT = config.ACCENT
 
-
-# def get_plot(theme=THEME, accent_base_color=ACCENT):
 def get_plot():
     """Returns a PyVista Plotter"""
-    plotter = pv.Plotter()  # we define a pyvista plotter
-    # if theme == "dark":
-    #     plotter.background_color = (0.13, 0.13, 0.13)
-    # else:
-    #    plotter.background_color = (0.97, 0.97, 0.97)
-    plotter.background_color = (0.97, 0.97, 0.97)
-
-    # we create a `VTK` panel around the render window
-    pvcylinder = pv.Cylinder(resolution=8, direction=(0, 1, 0))
-    # cylinder_actor = plotter.add_mesh(pvcylinder, color=accent_base_color, smooth_shading=True)
-    cylinder_actor = plotter.add_mesh(pvcylinder, smooth_shading=True)
-    cylinder_actor.RotateX(30.0)
-    cylinder_actor.RotateY(-45.0)
-    plotter.add_mesh(
-        pv.Sphere(theta_resolution=8, phi_resolution=8, center=(0.5, 0.5, 0.5)),
-        # color=accent_base_color,
-        smooth_shading=True,
-    )
+    plotter = pv.Plotter(shape=(3, 3))
+    # Top row
+    plotter.subplot(0, 0)
+    plotter.add_mesh(cyl, color="tan", show_edges=True)
+    plotter.subplot(0, 1)
+    plotter.add_mesh(arrow, color="tan", show_edges=True)
+    plotter.subplot(0, 2)
+    plotter.add_mesh(sphere, color="tan", show_edges=True)
+    # Middle row
+    plotter.subplot(1, 0)
+    plotter.add_mesh(plane, color="tan", show_edges=True)
+    plotter.subplot(1, 1)
+    plotter.add_mesh(line, color="tan", line_width=3)
+    plotter.subplot(1, 2)
+    plotter.add_mesh(box, color="tan", show_edges=True)
+    # Bottom row
+    plotter.subplot(2, 0)
+    plotter.add_mesh(cone, color="tan", show_edges=True)
+    plotter.subplot(2, 1)
+    plotter.add_mesh(poly, color="tan", show_edges=True)
+    plotter.subplot(2, 2)
+    plotter.add_mesh(disc, color="tan", show_edges=True)
+    # Render all of them
     return plotter.ren_win
 
 
