14253c0100ace3af5c3cfa3a44d8e60124c6a979
Tetsuo Koyama <tkoyama010@gmail.com>
2023-04-30 22:46:15 +0900

    Add display_graph_button

--- online-fem/717b0c78c00a9de9bd5a.py
+++ online-fem/717b0c78c00a9de9bd5a.py
@@ -11,3 +11,10 @@ filename_input.servable()
 
 load_file_button = pn.widgets.Button(name="ファイル読み込み", button_type="primary")
 load_file_button.servable()
+
+x_axis_select = pn.widgets.Select(name="X軸")
+y_axis_select = pn.widgets.Select(name="Y軸")
+display_graph_button = pn.widgets.Button(
+    name="グラフ描画", button_type="primary", disabled=True
+)
+display_graph_button.servable()
