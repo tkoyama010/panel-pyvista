8da5ccb830f285f1afa50576058170908606b17d
Tetsuo Koyama <tkoyama010@gmail.com>
2023-05-13 17:24:40 +0900

    Add animation

--- online-fem/hello-player.py
+++ online-fem/hello-player.py
@@ -1,41 +1,65 @@
 import numpy as np
+import panel as pn
+import param
 import pyvista as pv
+from IPython.display import IFrame
 
-x = np.arange(-10, 10, 0.5)
-y = np.arange(-10, 10, 0.5)
-x, y = np.meshgrid(x, y)
-r = np.sqrt(x**2 + y**2)
-z = np.sin(r)
 
-# Create and structured surface
-grid = pv.StructuredGrid(x, y, z)
+def handler(viewer, src, **kwargs):
+    return IFrame(src, "100%", "1000px")
 
-# Create a plotter object and set the scalars to the Z height
-plotter = pv.Plotter(notebook=False, off_screen=False)
-plotter.add_mesh(
-    grid,
-    scalars=z.ravel(),
-    lighting=False,
-    show_edges=True,
-    scalar_bar_args={"title": "Height"},
-    clim=[-1, 1],
-)
 
-# Open a gif
-plotter.show(interactive_update=True)
+class PyVistaViewer(param.Parameterized):
+    x = np.arange(-10, 10, 0.5)
+    y = np.arange(-10, 10, 0.5)
+    x, y = np.meshgrid(x, y)
+    r = np.sqrt(x**2 + y**2)
+    z = np.sin(r)
 
-pts = grid.points.copy()
+    # Create and structured surface
+    grid = pv.StructuredGrid(x, y, z)
 
-# Update Z and write a frame for each updated position
-nframe = 150
-for phase in np.linspace(0, 10 * np.pi, nframe + 1)[:nframe]:
-    z = np.sin(r + phase)
-    pts[:, -1] = z.ravel()
-    plotter.update_coordinates(pts, render=False)
-    plotter.update_scalars(z.ravel(), render=False)
+    # Create a plotter object and set the scalars to the Z height
+    plotter = pv.Plotter(notebook=True, off_screen=False)
+    plotter.add_mesh(
+        grid,
+        scalars=z.ravel(),
+        lighting=False,
+        show_edges=True,
+        scalar_bar_args={"title": "Height"},
+        clim=[-1, 1],
+    )
+    nframe = 150
+    step = param.Integer(default=1, bounds=(0, nframe))
 
-    # Write a frame. This triggers a render.
-    plotter.update()
+    value = np.linspace(0, 10 * np.pi, nframe + 1)
 
-# Closes and finalizes movie
-plotter.close()
+    @param.depends("step")
+    def view(self):
+        pts = self.grid.points.copy()
+
+        # Update Z and write a frame for each updated position
+        z = np.sin(self.r + self.value[self.step])
+        pts[:, -1] = z.ravel()
+        self.plotter.update_coordinates(pts, render=False)
+        self.plotter.update_scalars(z.ravel(), render=False)
+
+        # Write a frame. This triggers a render.
+        # self.plotter.update()
+        # Open a gif
+        iframe = self.plotter.show(
+            jupyter_backend="trame",
+            jupyter_kwargs=dict(handler=handler),
+            return_viewer=True,
+            interactive_update=True,
+        )
+        return iframe
+
+
+viewer = PyVistaViewer(name="PyVista Viewer")
+
+pn.extension()
+pn.Column(
+    pn.Param(viewer.param, widgets={"step": pn.widgets.Player}),
+    pn.panel(viewer.view, width=1000),
+).show()
